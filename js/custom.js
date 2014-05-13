b27647647cddde3bc1ab10608857fcab2ddc866d
Eric Johnson <edj.boston@gmail.com>
2014-05-13 03:10:25 -0700

    Markup, styles, and JS

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	// Open external links in new window
+	$('a.external').click(function(e) {
+		var url = $(this).attr('href');
+		window.open(url, 'new');
+		return false;
+	});
+
+});
