ac430677ed55ac512478f4a63197ff1546817c1c
Eric Johnson <edj.boston@gmail.com>
2015-11-03 23:38:34 -0800

    Switching from static index to built version

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 // External dependencies
-var clean  = require('gulp-clean'),
-	del  = require('del'),
-    gulp = require('gulp');
+var del  = require('del'),
+	fs  = require('fs'),
+    gulp = require('gulp'),
+    hb     = require('gulp-compile-handlebars'),
+    md = require('marked'),
+	moment = require('moment');
 
 // Catchall to copy static files to build
 gulp.task('static', ['clean'], function() {
@@ -14,10 +17,28 @@ gulp.task('clean', function() {
     return del('build');
 });
 
+// Compile HB template
+gulp.task('views', ['clean'], function() {
+
+	md.setOptions({gfm: true});
+
+	var data = {
+		title : 'MatchstickJS',
+		year : moment().format('YYYY'),
+		timestamp : moment().format('YYYY-MM-DD-HH-mm-ss'),
+		readme : md.parse(fs.readFileSync('node_modules/void/README.md', 'utf-8'))
+	};
+
+	return gulp.src('views/*.html')
+		.pipe(hb(data))
+		.pipe(gulp.dest('build'));
+});
+
 // Perform a build
 gulp.task('build', [
     'clean',
-    'static'
+    'static',
+    'views'
 ]);
 
 // What to do when you run `$ gulp`
