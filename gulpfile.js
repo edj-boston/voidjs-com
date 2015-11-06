037ceb8323fb9cccf6590692a91182a00ac78bbc
Eric Johnson <edj.boston@gmail.com>
2015-11-05 18:00:47 -0800

    moving views, static. js, and css to assets dir. Also building/minifying js/css in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,54 @@
 // External dependencies
-var del  = require('del'),
+var concat = require('gulp-concat'),
+	del  = require('del'),
 	fs  = require('fs'),
     gulp = require('gulp'),
     hb     = require('gulp-compile-handlebars'),
     md = require('marked'),
-	moment = require('moment');
+    minify = require('gulp-minify-css'),
+	moment = require('moment'),
+	uglify = require('gulp-uglify');
+
 
 // Catchall to copy static files to build
 gulp.task('static', ['clean'], function() {
-    return gulp.src('static/**')
+    return gulp.src('assets/static/**')
         .pipe(gulp.dest('build'));
 });
+
 
 // Clean the build dir
 gulp.task('clean', function() {
     return del('build');
 });
+
+
+// Minify and combine all JavaScript
+gulp.task('scripts', ['clean'], function() {
+	return gulp.src([
+			'assets/js/jquery-1.11.0.min.js',
+			'assets/js/bootstrap.min.js',
+			'assets/js/custom.js',
+			'assets/js/ga.js'
+		])
+		.pipe(concat('all.min.js'))
+		.pipe(uglify({preserveComments:'some'}))
+		.pipe(gulp.dest('build/js'));
+});
+
+
+// Minify and combine all CSS
+gulp.task('styles', ['clean'], function() {
+	return gulp.src([
+			'assets/css/bootstrap.min.css',
+			'assets/css/custom.css',
+			'assets/css/font-awesome.min.css'
+		])
+		.pipe(minify())
+		.pipe(concat('all.min.css'))
+		.pipe(gulp.dest('build/css'));
+});
+
 
 // Compile HB template
 gulp.task('views', ['clean'], function() {
@@ -23,7 +56,6 @@ gulp.task('views', ['clean'], function() {
 	md.setOptions({gfm: true});
 
 	var data = {
-		title : 'MatchstickJS',
 		year : moment().format('YYYY'),
 		timestamp : moment().format('YYYY-MM-DD-HH-mm-ss'),
 		readme : md.parse(fs.readFileSync('node_modules/void/README.md', 'utf-8'))
@@ -34,10 +66,13 @@ gulp.task('views', ['clean'], function() {
 		.pipe(gulp.dest('build'));
 });
 
+
 // Perform a build
 gulp.task('build', [
     'clean',
     'static',
+    'scripts',
+    'styles',
     'views'
 ]);
 
